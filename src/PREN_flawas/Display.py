71c8d50fb8a7d8a239d8c9018fbe0e3b85983a0d
flawas <32436687+flawas@users.noreply.github.com>
2024-02-29 20:20:44 +0100

    Updated display functionality

--- src/PREN_flawas/Display.py
+++ src/PREN_flawas/Display.py
@@ -2,15 +2,6 @@ import os, sys, logging, time
 from waveshare_epd import epd1in54_V2
 from PIL import Image, ImageDraw, ImageFont
 
-def __init__(epd, fontTTC, backgroundBMP ):
-    logging.info("Display init")
-    font = ImageFont.truetype(os.path.join(fontTTC), 16)
-    epd.init(1)
-    background = Image.open(os.path.join(backgroundBMP))
-    epd.displayPartBaseImage(epd.getbuffer(background))
-
-    # epd.init(1) # into partial refresh mode
-    ImageDraw.Draw(background)
 
 def clearDisplay(epd):
     logging.info("Display clear")
@@ -50,15 +41,14 @@ def shutdownDisplay(epd):
     epd.Clear(0xFF)
     epd.sleep()
 
-def drawInitialDisplay(epd, background):
+def drawInitialDisplay(epd, background, backgroundmodified, font):
     logging.info("Display draw initial display")
     try:
-        epd.init(1)
-        updateDisplay(epd, 10, 10, 'PREN TEAM 33', backgroundBMP=background)
+        updateDisplay(epd, 10, 10, 'PREN TEAM 33', background, backgroundmodified, font)
         # self.updateDisplay(10, 30, 'Initialisierung')
-        updateDisplay(epd, 10, 80, 'Beanspruchte Zeit', backgroundBMP=background)
+        updateDisplay(epd, 10, 80, 'Beanspruchte Zeit', background, backgroundmodified, font)
         # self.updateDisplay(10, 100, 'Sekunden')
-        updateDisplay(epd, 10, 150, 'Stromverbrauch', backgroundBMP=background)
+        updateDisplay(epd, 10, 150, 'Stromverbrauch', background, backgroundmodified, font)
         # self.updateDisplay(10, 170, 'kW')
 
     except IOError as e:
@@ -69,14 +59,10 @@ def drawInitialDisplay(epd, background):
         epd1in54_V2.epdconfig.module_exit()
         exit()
 
-def updateDisplay(epd, x, y, text, backgroundBMP):
-    logging.info("Display update: x:" + str(x) + ", y: " + str(y) + ", text: " + str(text))
+def updateDisplay(epd, x, y, text, backgroundBMP, backgroundModified, font):
     try:
-        image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-        epd.init(1)
-        background = Image.open(backgroundBMP)
-
-        draw = ImageDraw.Draw(image)
+        background = Image.open(os.path.join(backgroundModified))
+        draw = ImageDraw.Draw(background)
         draw.rectangle((x, y, 200, y + 20), fill=0)
         newimage = background.crop([x, y, 200, y + 20])
         background.paste(newimage, (x, y))
@@ -87,13 +73,16 @@ def updateDisplay(epd, x, y, text, backgroundBMP):
         background.paste(newimage, (x, y))
         epd.displayPart(epd.getbuffer(background))
 
-        draw.text((x, y), text, fill=0)
+        fontdisplay = ImageFont.truetype(font, 16)
+
+        draw.text((x, y), text, font=fontdisplay, fill=0)
         newimage = background.crop([x, y, 200, y + 20])
 
         background.paste(newimage, (x, y))
         epd.displayPart(epd.getbuffer(background))
 
-        background.save("background_modified.png")
+        background.save("pic/background_modified.bmp")
+
 
     except IOError as e:
         logging.error(e)
@@ -102,7 +91,3 @@ def updateDisplay(epd, x, y, text, backgroundBMP):
         logging.warning("ctrl + c:")
         epd1in54_V2.epdconfig.module_exit()
         exit()
-
-def loop(self):
-    self.drawPicture('qrcode.bmp')
-    self.clearDisplay()
