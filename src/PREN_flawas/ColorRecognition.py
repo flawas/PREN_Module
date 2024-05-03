ca46e4f4ae5b1aee4bf554b741ac763f26c4fbae
flawas <32436687+flawas@users.noreply.github.com>
2024-05-03 09:53:29 +0200

    Changed Cases A to D

--- src/PREN_flawas/ColorRecognition.py
+++ src/PREN_flawas/ColorRecognition.py
@@ -86,7 +86,7 @@ def open_camera_profile(ip_address, username, password, profile, screenshotName)
                 average_color_bot_left_left >= lower_white).all() and (
                 average_color_bot_left_left <= upper_white).all():
             cap.release()
-            __pos[0] = 1
+            __pos[0] = "A"
             cv2.imwrite(screenshotName + ".png", frame)
             return True
 
@@ -97,7 +97,7 @@ def open_camera_profile(ip_address, username, password, profile, screenshotName)
                 average_color_bot_right_right >= lower_white_right).all() and (
                 average_color_bot_right_right <= upper_white_right).all():
             cap.release()
-            __pos[0] = 2
+            __pos[0] = "B"
             cv2.imwrite(screenshotName + ".png", frame)
             return True
 
@@ -106,7 +106,7 @@ def open_camera_profile(ip_address, username, password, profile, screenshotName)
                 average_color_top_mid_left >= lower_white).all() and (
                 average_color_top_mid_left <= upper_white).all() and (average_color_bot_left_left <= lower_white).all():
             cap.release()
-            __pos[0] = 3
+            __pos[0] = "C"
             cv2.imwrite(screenshotName + ".png", frame)
             return True
 
@@ -116,7 +116,7 @@ def open_camera_profile(ip_address, username, password, profile, screenshotName)
                 average_color_top_mid_right <= upper_white).all():
             cap.release()
             cv2.imwrite(screenshotName + ".png", frame)
-            __pos[0] = 4
+            __pos[0] = "D"
             return True
 
         # Zur Entwicklung: Frame anzeigen
