baf6495f2c340e7e101eb3a3e11e3ea72a8ed95c
flawas <32436687+flawas@users.noreply.github.com>
2024-06-20 09:38:24 +0200

    Added Sys

--- src/PREN_flawas/Sys.py
+++ src/PREN_flawas/Sys.py
@@ -1,5 +1,5 @@
 import os, time
 def wait_until_file_exists(file_path, sleep_interval=1):
     while not os.path.exists(file_path):
-        time.sleep(0.01)
+        time.sleep(sleep_interval)
     return True
